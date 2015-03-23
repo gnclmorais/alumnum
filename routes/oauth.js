fa6a47fa157a1a89b8c61b62042c02a0bfbd1c9c
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-03-23 18:45:27 -0400

    Dynamic OAuth skeleton.

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,15 +1,27 @@
 var express = require('express'),
     OAuth = require('oauth').OAuth,
     querystring = require('querystring'),
-    router = express.Router();
+    router  = express.Router(),
+    keyfile = require('../config/api_keys');
+
+function getKeys(req, res, next) {
+	var service = req.params.service,
+	    keys    = keyfile[service];
+	if (keys) return keys;
+	next( new Error( "No API keys for service '"+service+"'.  Please add to config/api_keys.js." ) );
+}
 
 router.get('/:service/login', function(req, res, next) {
-  res.render('index', { title: 'Login With '+req.params.service.toUpperCase() });
+  var service = req.params.service,
+      keys    = getKeys(req, res, next);
+  res.render('index', { title: 'Login With '+service.toUpperCase() });
   res.end();
 });
 
 router.get('/:service/callback', function(req, res, next) {
-  res.render('index', { title: 'Callback For '+req.params.service.toUpperCase() });
+  var service = req.params.service,
+      keys    = getKeys(req, res, next);
+  res.render('index', { title: 'Callback For '+service.toUpperCase() });
   res.end();
 });
 
