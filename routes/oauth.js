a4d2f203ce4e992f2376043ef4fc80559519461d
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-03-23 21:29:49 -0400

    OAuth test stuff.

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    OAuth = require('oauth').OAuth,
+    OAuth2 = require('oauth').OAuth2,
     querystring = require('querystring'),
     router  = express.Router(),
     keyfile = require('../config/api_keys');
@@ -24,5 +24,38 @@ router.get('/:service/callback', function(req, res, next) {
   res.render('index', { title: 'Callback For '+service.toUpperCase() });
   res.end();
 });
+
+function testOauth(service) {
+	var keys = keyfile[service];
+	var scope = querystring.escape("https://www.google.com/m8/feeds/")
+	if (!keys) throw "No keys for service: "+service;
+	console.log(scope)
+	var oauth2 = new OAuth2(
+	  keys.client_id,
+      keys.secret, 
+      'https://accounts.google.com/o/', 
+      'https://accounts.google.com/o/oauth2/token',
+      'oauth2/token', 
+      null
+    );
+    var oauth2 = new OAuth2(
+    	keys.client_id,//clientId,
+    	keys.secret,//clientSecret,
+    	'https://accounts.google.com/o/',//baseSite,
+    	'oauth2/auth?scope='+scope,//authorizePath,
+    	'oauth2/token?scope='+scope,//accessTokenPath,
+    	{'scope':scope} //customHeaders
+    );
+    oauth2.getOAuthAccessToken(
+      '',
+      {'grant_type':'client_credentials'},
+      function (e, access_token, refresh_token, results){
+      	console.log('error:', e);
+        console.log('bearer: ',access_token);
+      }
+    );
+}
+
+testOauth('google');
 
 module.exports = router;
