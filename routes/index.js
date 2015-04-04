ebf807b3405c96d917e77cf43bfff51e66032d72
Gonçalo Morais <gnclmorais@gmail.com>
2015-04-04 15:00:26 -0400

    Title fix.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Alumnum'
+  });
 });
 
 module.exports = router;
